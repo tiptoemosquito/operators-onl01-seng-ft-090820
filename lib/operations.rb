72096d6bc453632eea8be292d8e79b41784ef803
Christina Howard <clhoward33@icloud.com>
2020-09-10 17:55:28 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,9 @@
 def unsafe?(speed)
-	if speed < 40
-		false
-	else speed > 60
-		true
-	elsif speed ==
-		false
-	end
-
+   if speed < "40" || speed > "60"
+     true
+   else
+  	false
+  end
 end
 
 def not_safe?(speed)
